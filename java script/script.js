d4ef3fcfd64d1004a41c2d267268d0d8456ee68b
DhruvPrajapati-1918 <dhruvprajapati1918@gmail.com>
2026-03-03 17:43:27 +0530

    Update script.js

--- java script/script.js
+++ java script/script.js
@@ -30,3 +30,53 @@ form.addEventListener("submit",function(event) {
         console.log("searching for: " + value);
     }
 });
+
+const toastContainer = document.getElementById("toast-container");
+const successBtn = document.querySelector(".Trigger");
+const errorBtn = document.querySelector(".Trigger2");
+
+function createToast(type, message) {
+
+    const toast = document.createElement("div");
+    toast.classList.add("toast", type);
+
+    let icon = type === "success"
+        ? '<i class="fa-solid fa-circle-check"></i>'
+        : '<i class="fa-solid fa-triangle-exclamation"></i>';
+
+    toast.innerHTML = `
+        ${icon}
+        <span>${message}</span>
+        <button class="close-btn">&times;</button>
+    `;
+
+    toastContainer.appendChild(toast);
+
+    setTimeout(() => {
+        toast.classList.add("show");
+    }, 100);
+
+    setTimeout(() => {
+        removeToast(toast);
+    }, 3000);
+
+    toast.querySelector(".close-btn").addEventListener("click", () => {
+        removeToast(toast);
+    });
+}
+
+function removeToast(toast) {
+    toast.classList.remove("show");
+
+    setTimeout(() => {
+        toast.remove(); 
+    }, 400);
+}
+
+successBtn.addEventListener("click", () => {
+    createToast("success", "Project saved successfully!");
+});
+
+errorBtn.addEventListener("click", () => {
+    createToast("error", "Failed to fetch data!");
+});
